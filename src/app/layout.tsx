f9319592eab991e497af2930fbf3965c4f8506c9
CvetomirKuzmanov <cveto.kuzmanov@gmail.com>
2025-04-28 19:23:36 +0300

    Added Header Componenet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/static/components/Header";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
